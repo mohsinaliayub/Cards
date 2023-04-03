aac8860e124d6c4fe4c652ff276c8ddb1480dbcf
Mohsin Ali Ayub <meismosin@gmail.com>
2023-04-03 22:18:01 +0200

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Mohsin Ali Ayub on 03.04.23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
